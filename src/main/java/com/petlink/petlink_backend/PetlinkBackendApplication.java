5ea686f40d05b460c107d9faddf9bb08f46e33d7
Developer <danielj.chavarriz@gmail.com>
2025-10-06 18:24:00 -0500

    Connect backend to Railway Postgres

--- src/main/java/com/petlink/petlink_backend/PetlinkBackendApplication.java
+++ src/main/java/com/petlink/petlink_backend/PetlinkBackendApplication.java
@@ -3,11 +3,12 @@ package com.petlink.petlink_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.petlink.petlink_backend")
+@SpringBootApplication
 public class PetlinkBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetlinkBackendApplication.class, args);
+		SpringApplication app = new SpringApplication(PetlinkBackendApplication.class);
+		app.setAdditionalProfiles("local");
+		app.run(args);
 	}
-
 }
