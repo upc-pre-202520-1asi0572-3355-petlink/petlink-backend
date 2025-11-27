2f0961ac08a33896c3ff3a847ecf412b393dfd8e
Cristopher Rondon Añaños <89040296+Cris200399@users.noreply.github.com>
2025-11-26 20:45:11 -0500

    Update CORS configuration to allow specific origins

--- src/main/java/com/petlink/petlink_backend/config/CorsConfig.java
+++ src/main/java/com/petlink/petlink_backend/config/CorsConfig.java
@@ -15,6 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
+                            "*",
                                 "http://localhost:4200",
                                 "https://petwatch.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
